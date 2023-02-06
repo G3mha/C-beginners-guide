db91532412e547f4da6c87d9f9b57540034e1844
Enricco Gemha <riccogemha@gmail.com>
2023-02-06 17:51:06 -0300

    beggining

---
+++ code/02-praticando/tarefa6.c
@@ -0,0 +1,18 @@
+// Configurações do mutirão. Não mexer.
+#include "../macros_correcoes.h"
+#define LABNUM "lab2-"
+#define SOLUTIONFILE "tarefa6.c"
+
+#include <stdio.h>
+
+// TODO implemente sua função aqui
+void fizzbuzz(int n) {
+}
+
+// TODO implemente aqui um programa que chama
+// a função que você faz, passando como
+// parâmetro um inteiro que foi dado pelo usuário
+int main() {
+
+    return 0;
+}
---
+++ code/02-praticando/tarefa7.c
@@ -0,0 +1,18 @@
+// Configurações do mutirão. Não mexer.
+#include "../macros_correcoes.h"
+#define LABNUM "lab2-"
+#define SOLUTIONFILE "tarefa7.c"
+
+#include <stdio.h>
+
+// TODO implemente sua função aqui
+void arvore(int n) {
+}
+
+// TODO implemente aqui um programa que chama
+// a função que você faz, passando como
+// parâmetro um inteiro que foi dado pelo usuário
+int main() {
+
+    return 0;
+}
